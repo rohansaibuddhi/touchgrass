09fced80b0f2891fbbdb5710ab762bede7c24626
Rohan Sai Buddhi <rohan31sai@gmail.com>
2024-11-01 16:10:43 -0500

    Removed comments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,10 +8,3 @@ export default withMT({
   },
   plugins: [],
 });
-// export default {
-// 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-// 	theme: {
-// 		extend: {},
-// 	},
-// 	plugins: [],
-// }
