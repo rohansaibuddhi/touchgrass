2e9b479e64ff2ce1fe0874f0d2c4153682a5d7f3
Umang Chaudhari <uchaud8@uic.edu>
2024-11-14 14:21:19 -0600

    Disable non mvp feature

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -2,40 +2,34 @@ import Button from "../layouts/Button";
 import TimelineMarker from "../layouts/TimelineMarker";
 
 export interface Activity {
-    id: number,
-    summary: string,
-    location: string,
-    completed: boolean
+  id: number;
+  summary: string;
+  location: string;
+  completed: boolean;
 }
 
 interface HistoryProps {
-    activities: Activity[],
-    handleRetry: Function
+  activities: Activity[];
+  handleRetry: Function;
 }
 
-export default function History({activities, handleRetry}: HistoryProps) {
+export default function History({ activities, handleRetry }: HistoryProps) {
   return (
-    <div id="history" className="w-full max-w-md justify-self-center mt-20 bg-white shadow-md rounded px-8 pt-6 pb-8">
-        <ol className="relative border-s border-gray-700">
-            {activities.map(item => (
-                <li key="{item.id}" className="mb-10 ms-4">
-                    <TimelineMarker/>
-                    <h3>
-                        {item.location}
-                    </h3>
-                    <p className="text-base font-normal text-gray-400">
-                        {item.summary}
-                    </p>
-                    {
-                        !item.completed && 
-                        <Button 
-                            text="Try Again"
-                            clickHandler={evt => handleRetry(evt.target)}
-                            alignment="mt-2"/>
-                    }
-                </li>
-            ))}
-        </ol>
+    <div
+      id="history"
+      className="w-full max-w-md justify-self-center mt-20 bg-white shadow-md rounded px-8 pt-6 pb-8"
+    >
+      <ol className="relative border-s border-gray-700">
+        {activities.map((item) => (
+          <li key="{item.id}" className="mb-10 ms-4">
+            <TimelineMarker />
+            <h3>{item.location}</h3>
+            <p className="text-base font-normal text-gray-400">
+              {item.summary}
+            </p>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
